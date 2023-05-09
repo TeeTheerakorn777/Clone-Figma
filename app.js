dd03d9bf3063b15ce758662a7d337ad52ca64d92
theerakorn <theerakon.dev@gmail.com>
2023-05-09 08:22:45 +0700

    Last

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const btnMenu = document.querySelector('img[src="./Img/btn-menu.png"]');
 const ulMenu = document.querySelector('ul');
 
-btnMenu.addEventListener('click', function() {
+btnMenu.addEventListener('click', function () {
   if (ulMenu.style.display === 'none') {
     ulMenu.style.display = 'flex';
   } else {
@@ -9,23 +9,30 @@ btnMenu.addEventListener('click', function() {
   }
 });
 
+document.addEventListener('click', function (event) {
+  const target = event.target;
+  if (!target.closest('ul') && !target.closest('img[src="./Img/btn-menu.png"]')) {
+    ulMenu.style.display = 'none';
+  }
+});
 
-    const prevBtn = document.getElementById("prev-btn");
-    const nextBtn = document.getElementById("next-btn");
-    const slides = document.querySelectorAll(".slide, .slide1, .slide2");
-    let currentSlide = 0;
 
-    function showSlide(n) {
-        slides[currentSlide].style.display = "none";
-        currentSlide = (n + slides.length) % slides.length;
-        slides[currentSlide].style.display = "flex";
-    }
+const prevBtn = document.getElementById("prev-btn");
+const nextBtn = document.getElementById("next-btn");
+const slides = document.querySelectorAll(".slide, .slide1, .slide2");
+let currentSlide = 0;
 
-    prevBtn.addEventListener("click", () => {
-        showSlide(currentSlide - 1);
-    });
+function showSlide(n) {
+  slides[currentSlide].style.display = "none";
+  currentSlide = (n + slides.length) % slides.length;
+  slides[currentSlide].style.display = "flex";
+}
 
-    nextBtn.addEventListener("click", () => {
-        showSlide(currentSlide + 1);
-    });
+prevBtn.addEventListener("click", () => {
+  showSlide(currentSlide - 1);
+});
+
+nextBtn.addEventListener("click", () => {
+  showSlide(currentSlide + 1);
+});
 
